4840457af6cfd97b06222f9a34389e2c0747ce68
bitifychen <bitify@mail.fcu.edu.tw>
2022-04-14 23:16:22 +0800

    220414-Red dot !

--- main.js
+++ main.js
@@ -3,19 +3,31 @@ const btnMenu = document.querySelector('header.main button.menu')
 const menu = document.querySelector('header.main')
 btnMenu.addEventListener('click',menuOpen)
 
+/*Nav-desktop*/
+const navBar = document.querySelector('.main-nav .links')
+const navLinks = document.querySelectorAll('.main-nav .link')
+const dot = document.querySelector('.dot')
+navBar.addEventListener("mouseover", () =>{ dot.classList.add('moving');})
+navBar.addEventListener("mouseout", () =>{ dot.classList.remove('moving');})
+navLinks.forEach((link) =>{ 
+  link.addEventListener("mouseover", () =>
+  { 
+    dot.classList.add('moving');
+    dot.style.setProperty('--move', link.offsetLeft - dot.offsetLeft + 'px');
+  });
+})
 
-/*Safari*/
+
+
+
 function menuOpen(){
   menu.classList.toggle('active')
 }
 
+/*Safari*/
 function safariHacks() {
   let windowsVH = window.innerHeight / 100;
   document.querySelector('main').style.setProperty('--vh', windowsVH + 'px');
-  window.addEventListener('resize', function() {
-      document.querySelector('main').style.setProperty('--vh', windowsVH + 'px');
-  });
 }
-
-
+window.addEventListener('resize', safariHacks);
 safariHacks();
