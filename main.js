3b01c9e8e0fc956b095941140b61059cc26333e7
bitifyChen <43808882+bitifyChen@users.noreply.github.com>
2022-04-14 22:07:45 +0800

    220414-safari100VH、navbar大小、index風格

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
-/*Nav*/
+/*Nav-mobile*/
 const btnMenu = document.querySelector('header.main button.menu')
 const menu = document.querySelector('header.main')
 btnMenu.addEventListener('click',menuOpen)
 
+
+/*Safari*/
 function menuOpen(){
   menu.classList.toggle('active')
 }
+
+function safariHacks() {
+  let windowsVH = window.innerHeight / 100;
+  document.querySelector('main').style.setProperty('--vh', windowsVH + 'px');
+  window.addEventListener('resize', function() {
+      document.querySelector('main').style.setProperty('--vh', windowsVH + 'px');
+  });
+}
+
+
+safariHacks();
