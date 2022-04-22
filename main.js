333ebc16893547154d9f18c13aae54406ec9b00e
bitifyChen <43808882+bitifyChen@users.noreply.github.com>
2022-04-22 15:56:55 +0800

    220422-(CSS)首頁專長、學生頁面

--- main.js
+++ main.js
@@ -32,28 +32,28 @@ if (dot) {
       dot.style.setProperty("--move", link.offsetLeft - dot.offsetLeft + "px");
     });
   });
-}
-/*--進頁面後判斷*/
-/*---跑一次---*/
-if (dotReload) {
-  navLinks.forEach((link) => {
-    /*---確認有已啟動*/
-    if (link.classList.contains("active")) {
-      dot.style.setProperty("--move", link.offsetLeft - dot.offsetLeft + "px");
-      dot.classList.add("with-position"); //無轉場時間，讓紅點不飄移
-      dot.classList.add("moving");
-    }
-    /*---判斷換頁後滑鼠在不在navbar */
-    document.querySelector("body").addEventListener("mousemove", () => {
-      if (!menu.matches(":hover")) {
-        dot.classList.remove("moving");
-        dot.classList.remove("with-position");
-      }
-    });
-  });
-  dotReload = false;
-}
 
+  /*--進頁面後判斷*/
+  /*---跑一次---*/
+  if (dotReload) {
+    navLinks.forEach((link) => {
+      /*---確認有已啟動*/
+      if (link.classList.contains("active")) {
+        dot.style.setProperty("--move", link.offsetLeft - dot.offsetLeft + "px");
+        dot.classList.add("with-position"); //無轉場時間，讓紅點不飄移
+        dot.classList.add("moving");
+      }
+      /*---判斷換頁後滑鼠在不在navbar */
+      document.querySelector("body").addEventListener("mousemove", () => {
+        if (!menu.matches(":hover")) {
+          dot.classList.remove("moving");
+          dot.classList.remove("with-position");
+        }
+      });
+    });
+    dotReload = false;
+  }
+}
 /*Back to Previous Page 返回上一頁*/
 const btnBack = document.querySelectorAll(".leave button.back");
 if (btnBack) {
